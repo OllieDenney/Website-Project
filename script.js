8ae680f01812fccdf7ab57f70fe47e0784e00ea6
OllieDenney <18odenney@wakatipu.school.nz>
2022-09-07 11:46:06 +1200

    7/9/22 - Further developed the image slideshow in the prints page although coming across problems with passing variables in JS.

--- script.js
+++ script.js
@@ -27,3 +27,33 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
+
+let slideIndex = 1;
+console.log(slideIndex)
+
+setInterval(plusSlides(slideIndex), 1000);
+
+function plusSlides(slideIndex) {
+  showSlides(slideIndex += 1);
+	console.log(slideIndex)
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
