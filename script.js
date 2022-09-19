c2589b267801bd4a81ebec0dbb1b38e9c1dce4a3
Ollie Denney <18odenney@wakatipu.school.nz>
2022-09-19 13:49:46 +1200

    19/9/22 - Major developments to the entirety of the website. The prints page has been finished and the automatic slideshow has been corrected and is working as should. The home page and collab page headers have been updated and the home page header is being finished as a transition is being added. The home page prints section has been finished with an automatic slideshow being added. The next step is to finish the collab section on the home page and finalise the bottom section on the home page. Then the website will be entirely complete and some minor refinements can be done adn then the implemenation of media queries can commence.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let slideIndex = 0;
-showSlides();
 
+
+window.onload = function(){
+	document.getElementById("header-text").style.opacity = 1;
+	setTimeout(function() {
+		document.getElementById("header-text").style.opacity = 0;
+	},3000)
+	setTimeout(function() {
+		document.getElementById("header-text").innerHTML = "Minaret";
+		document.getElementById("header-text").style.opacity = 1;
+	},3000)
+}
 
 window.onscroll = function(){
 	var a = document.getElementById("nav-container-fade");
@@ -30,21 +39,3 @@ function reveal() {
 }
 
 window.addEventListener("scroll", reveal);
-
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
