6071a39c3d79db5e60fc08904112ccb2b3209508
Ollie Denney <18odenney@wakatipu.school.nz>
2022-09-19 15:04:03 +1200

    19/9/22 - More major developments to the home page, finished the transitions for the home page, the home page now has an amazing loading in screen and great animations. This is now the finalised version of the nav bar until adaptation with media queries begins. Next step is to develop the collab section.

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 
 window.onload = function(){
 	document.getElementById("header-text").style.opacity = 1;
+	document.getElementById("breaker-text").style.opacity = 1;
+	document.getElementById("sub-text").style.opacity = 1;
 	setTimeout(function() {
 		document.getElementById("header-text").style.opacity = 0;
-	},3000)
-	setTimeout(function() {
-		document.getElementById("header-text").innerHTML = "Minaret";
-		document.getElementById("header-text").style.opacity = 1;
+		document.getElementById("breaker-text").style.opacity = 0;
+		document.getElementById("sub-text").style.opacity = 0;
+		document.getElementById("home-nav-link-1").style.color = "white";
+		document.getElementById("home-nav-link-2").style.color = "white";
+		document.getElementById("home-nav-link-3").style.color = "white";
+		document.getElementById("home-nav-link-4").style.color = "white";
+		document.getElementById("home-nav-link-5").style.color = "white";
+		document.getElementById("icon-image").src = "images/minaret-tab-icon-white.png";
+		document.getElementById("new-header-text").style.opacity = 1;
 	},3000)
 }
 
@@ -19,6 +26,29 @@ window.onscroll = function(){
 		a.style.background = "white";
 	}else{
 		a.style.background = "transparent";
+	}
+}
+
+window.onscroll = function(){
+	var a = document.getElementById("nav-container-fade");
+	var top = window.scrollY;
+	console.log(top);
+	if (top >= 1080){
+		a.style.background = "white";
+		document.getElementById("home-nav-link-1").style.color = "black";
+		document.getElementById("home-nav-link-2").style.color = "black";
+		document.getElementById("home-nav-link-3").style.color = "black";
+		document.getElementById("home-nav-link-4").style.color = "black";
+		document.getElementById("home-nav-link-5").style.color = "black";
+		document.getElementById("icon-image").src = "images/minaret-tab-icon.png";
+	}else{
+		a.style.background = "transparent";
+		document.getElementById("home-nav-link-1").style.color = "white";
+		document.getElementById("home-nav-link-2").style.color = "white";
+		document.getElementById("home-nav-link-3").style.color = "white";
+		document.getElementById("home-nav-link-4").style.color = "white";
+		document.getElementById("home-nav-link-5").style.color = "white";
+		document.getElementById("icon-image").src = "images/minaret-tab-icon-white.png";
 	}
 }
 
